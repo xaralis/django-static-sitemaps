955b3d94b6ffa7f0a37737380acff4d2a528a0d8
xaralis <xaralis@centrum.cz>
2014-08-28 09:52:26 +0200

    Make celery task optional fixes #24

--- static_sitemaps/tasks.py
+++ static_sitemaps/tasks.py
@@ -7,11 +7,13 @@ from static_sitemaps.generator import SitemapGenerator
 
 __author__ = 'xaralis'
 
-
-class GenerateSitemap(PeriodicTask):
-    run_every = timedelta(minutes=conf.CELERY_TASK_REPETITION)
-
-    def run(self, **kwargs):
-        generator = SitemapGenerator(verbosity=1)
-        generator.write()
+# Create class conditionally so the task can be bypassed when repetition 
+# is set to something which evaluates to False.
+if conf.CELERY_TASK_REPETITION:
+    class GenerateSitemap(PeriodicTask):
+        run_every = timedelta(minutes=conf.CELERY_TASK_REPETITION)
+    
+        def run(self, **kwargs):
+            generator = SitemapGenerator(verbosity=1)
+            generator.write()
 
