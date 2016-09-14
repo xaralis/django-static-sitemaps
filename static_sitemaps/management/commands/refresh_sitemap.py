8f8e826acd0aaed540d273719d0d99d079dd1673
Alexey <alexey@uzhva.com>
2016-09-14 09:15:36 +0700

    Fixed 1.10 compatiability

--- static_sitemaps/management/commands/refresh_sitemap.py
+++ static_sitemaps/management/commands/refresh_sitemap.py
@@ -1,14 +1,13 @@
-from django.core.management.base import NoArgsCommand
-
+from django.core.management.base import BaseCommand
 from static_sitemaps.generator import SitemapGenerator
 
 __author__ = 'xaralis'
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     command = None
     help = 'Generates sitemaps files to a predefined directory.'
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         generator = SitemapGenerator(int(options.get('verbosity', 0)))
         generator.write()
