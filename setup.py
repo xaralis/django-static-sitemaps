317d015d7158ef87a4037df0186d38b4250b9ba7
Rachel Tobin <rtobin@rover.com>
2017-10-03 13:46:32 -0700

    Specify management command as a package in setup

--- setup.py
+++ setup.py
@@ -13,7 +13,11 @@ setup(
     license='BSD',
     url='http://github.com/xaralis/django-static-sitemaps',
 
-    packages=('static_sitemaps',),
+    packages=(
+        'static_sitemaps',
+        'static_sitemaps.management',
+        'static_sitemaps.management.commands',
+        'static_sitemaps.templates'),
 
     include_package_data=True,
 
