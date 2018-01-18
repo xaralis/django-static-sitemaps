01b1048c3c63ae9e743e4e8abbc75f9ae44a8d06
iancubitt <ian.f.cubitt@gmail.com>
2018-01-18 15:05:52 +0100

    Add Django 2.0 support

--- static_sitemaps/generator.py
+++ static_sitemaps/generator.py
@@ -15,7 +15,10 @@ from django.core.exceptions import ImproperlyConfigured
 from django.core.files.base import ContentFile
 from django.core.files.storage import FileSystemStorage
 from django.core.paginator import EmptyPage, PageNotAnInteger
-from django.core.urlresolvers import reverse, NoReverseMatch
+try:
+    from django.core.urlresolvers import reverse, NoReverseMatch
+except ImportError:  # Django >= 2.0
+    from django.urls import reverse, NoReverseMatch
 from django.template import loader
 from django.utils import translation
 from django.utils.encoding import smart_str
